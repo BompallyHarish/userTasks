7c97b05a4cfa484969aab32242789de8660d580d
Bompally Harish <harishfebblar@gmail.com>
2024-06-04 23:33:29 +0530

    form changes.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -7,30 +7,37 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   templateUrl: './new-task.component.html',
   styleUrls: ['./new-task.component.css']
 })
-export class NewTaskComponent implements OnInit{
- 
+export class NewTaskComponent implements OnInit {
+
   @Output() close = new EventEmitter<void>()
   @Output() taskData = new EventEmitter<any>()
+
   @Input() userId !: string
+
+  public inPutTaskData !: { title: string, summary: string, date: Date, userId: string }
+  enteredTitle !:string
+  enteredSummary !: string
+  enteredDate!: Date;
+
   ngOnInit(): void {
     // console.log(this.userId)
+    this.inPutTaskData = {
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+      userId: this.userId
+    }
+    console.log('Input Data ', this.inPutTaskData)
   }
-  enteredTitle = '';
-  enteredSummary = '';
-  enteredDate!:Date;
-  inPutTaskData = {
-    title: this.enteredTitle,
-    summary: this.enteredSummary,
-    date: this.enteredDate,
-    userId: this.userId
-  }
+
+
   onCancel() {
     this.close.emit()
   }
   onSubmit() {
-    // console.log(this.inPutTaskData)
+    console.log(this.inPutTaskData)
     this.close.emit()
     this.taskData.emit(this.inPutTaskData)
-   
+
   }
 }
